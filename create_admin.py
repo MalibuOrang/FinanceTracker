3da5ab2a7da35bb9df365f0f64706b99f5e53965
Egor Feldsherov <raizap804@gmail.com>
2025-12-03 16:29:15 +0300

    add path ot settings create_admin file

--- create_admin.py
+++ create_admin.py
@@ -1,9 +1,20 @@
+import os
+import django
+
+# Указываем путь к настройкам вашего проекта
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "finance_project.settings"
+)  # замените my_project на имя вашего проекта
+
+# Инициализируем Django
+django.setup()
+
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "MrOrange*8004")
+    User.objects.create_superuser("admin", "admin@example.com", "yourpassword")
     print("Admin user created")
 else:
     print("Admin user already exists")
