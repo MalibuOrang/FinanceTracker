cb7e32d3fcf6265b6ec79a23cceca211779f8ede
Egor Feldsherov <raizap804@gmail.com>
2025-12-03 22:59:09 +0300

    delete js show_spinner

--- staticfiles/js/show_spinner.js
+++
@@ -1,8 +0,0 @@
-document.body.addEventListener('htmx:beforeRequest', function (evt) {
-    document.getElementById('spinner').classList.remove('hidden');
-});
-
-document.body.addEventListener('htmx:afterRequest', function (evt) {
-    document.getElementById('spinner').classList.add('hidden');
-});
-
